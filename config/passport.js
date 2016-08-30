94a7311e1db0ca3fb0d60dbd74ddbac8a5e1d8d9
XinleiStone <741447430@qq.com>
2016-08-30 23:24:42 +0800

    passport modify

--- config/passport.js
+++ config/passport.js
@@ -1,63 +1,44 @@
-
-// load all the things we need
+// 加载需要的模块
 var LocalStrategy = require('passport-local').Strategy;
-
-// load up the user model
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var dbconfig = require('./config');
 var connection = mysql.createConnection(dbconfig.connection);
 
 connection.query('USE ' + dbconfig.database);
-// expose this function to our app using module.exports
+
 module.exports = function(passport) {
 
-    // =========================================================================
-    // passport session setup ==================================================
-    // =========================================================================
-    // required for persistent login sessions
-    // passport needs ability to serialize and unserialize users out of session
-
-    // used to serialize the user for the session
+    // session
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
 
-    // used to deserialize the user
+    // 反序列化
     passport.deserializeUser(function(id, done) {
         connection.query("SELECT * FROM users WHERE id = ? ", [id], function(err, rows) {
             done(err, rows[0]);
         });
     });
 
-    // =========================================================================
-    // LOCAL SIGNUP ============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
     passport.use(
         'local-signup',
         new LocalStrategy({
-                // by default, local strategy uses username and password, we will override with email
                 usernameField: 'username',
                 passwordField: 'password',
-                passReqToCallback: true // allows us to pass back the entire request to the callback
+                passReqToCallback: true 
             },
             function(req, username, password, done) {
-                // find a user whose email is the same as the forms email
-                // we are checking to see if the user trying to login already exists
                 connection.query("SELECT * FROM users WHERE username = ?", [username], function(err, rows) {
                     if (err)
                         return done(err);
                     if (rows.length) {
-                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+                        return done(null, false, req.flash('signupMessage', '该用户名已注册'));
                     } else {
-                        // if there is no user with that username
-                        // create the user
+                       
                         var newUserMysql = {
                             username: username,
-                            password: bcrypt.hashSync(password, null, null) // use the generateHash function in our user model
+                            password: bcrypt.hashSync(password, null, null) 
                         };
 
                         var insertQuery = "INSERT INTO users ( username, password ) values (?,?)";
@@ -72,33 +53,24 @@ module.exports = function(passport) {
             })
     );
 
-    // =========================================================================
-    // LOCAL LOGIN =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
-
     passport.use(
         'local-login',
         new LocalStrategy({
-                // by default, local strategy uses username and password, we will override with email
                 usernameField: 'username',
                 passwordField: 'password',
-                passReqToCallback: true // allows us to pass back the entire request to the callback
+                passReqToCallback: true 
             },
-            function(req, username, password, done) { // callback with email and password from our form
+            function(req, username, password, done) { 
                 connection.query("SELECT * FROM users WHERE username = ?", [username], function(err, rows) {
                     if (err)
                         return done(err);
                     if (!rows.length) {
-                        return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                        return done(null, false, req.flash('loginMessage', '用户名不存在'));
                     }
 
-                    // if the user is found but the password is wrong
                     if (!bcrypt.compareSync(password, rows[0].password))
-                        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                        return done(null, false, req.flash('loginMessage', '密码错误')); 
 
-                    // all is well, return successful user
                     return done(null, rows[0]);
                 });
             })
